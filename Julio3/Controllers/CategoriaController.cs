18d10a0cc382d19fee7603dad3527e7ad2c9cbbd
Jorge Castro <jorge.castro.c@gmail.com>
2019-07-03 21:28:41 -0400

    ok.

---
+++ Julio3/Controllers/CategoriaController.cs
@@ -0,0 +1,38 @@
+﻿using Julio3.dal;
+using Julio3.ef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Julio3.Controllers
+{
+    public class CategoriaController : Controller
+    {
+       
+
+        public ActionResult Listar()
+        {
+            ViewBag.categorias=CategoriaDal.Listar();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Insertar()
+        {
+            //  formulario por defecto
+            var modelo=new Categoria();
+            return View(modelo);
+        }
+        [HttpPost] // alt+shift+f10
+        public ActionResult Insertar(Categoria modelo)
+        {
+            // insertar el valor en la base de datos
+            ViewBag.id= CategoriaDal.Insertar(modelo);
+            // redireccionar a la pagina de listado
+            Response.Redirect("/Categoria/Listar");
+
+            return View(modelo);
+        }
+    }
+}
---
+++ Julio3/dal/CategoriaDal.cs
@@ -0,0 +1,55 @@
+﻿using Julio3.ef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Julio3.dal
+{
+    public class CategoriaDal
+    {
+        public static List<Categoria> Listar()
+        {
+            var lista=new List<Categoria>();
+            using(var db=new Model1())
+            {
+                lista=db.Categoria.ToList();
+            }
+            return lista;
+        }
+        public static int Insertar(Categoria nueva)
+        {
+            int categoriaId=0;
+            using (var db = new Model1())
+            {
+                db.Categoria.Add(nueva);
+                db.SaveChanges();
+                categoriaId=nueva.CategoriaId;
+            }
+            return categoriaId; // devuelve la identidad
+        }
+        public static void Modificar(Categoria cat)
+        {
+            using (var db = new Model1())
+            {
+                // leemos la categoria desde la base de datos
+                var catAntigua=db.Categoria
+                    .FirstOrDefault(c => c.CategoriaId==cat.CategoriaId);
+                // modificamos la categoria leida usando el argumento (cat)
+                catAntigua.Nombre=cat.Nombre;
+                catAntigua.CategoriaId=cat.CategoriaId; // redundante
+                db.SaveChanges(); // guarfamos los cambios.
+            }
+        } // fin modificar
+        public static Categoria Leer(int CategoriaId)
+        {
+            var cat=new Categoria();
+            using (var db = new Model1())
+            {
+                cat=db.Categoria.FirstOrDefault(c =>c.CategoriaId==CategoriaId);
+            }
+            return cat;
+        } // fin leer
+
+    }
+}
